95da865a9b8d693575a16d94f0f627bce1a78c72
CodemakerPHS <pauline.ward5@phs.scot>
2024-09-27 10:35:50 +0100

    Adding files for code from HOPR book

---
+++ code/hopr_ch2_functions.R
@@ -0,0 +1,16 @@
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Working through HOPR - Hands-On Programming with R   
+# Ch. 2
+# https://rstudio-education.github.io/hopr/basics.html#objects
+#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+library(janitor)
+
+# 2.2 Objects 
+# Even simple variables that you save are objects. 
+
+a <- 3
+<- <- <- <- <- <- <- 
+
+die <- 1:6
