c30fc86fb3d9a7c9b217691e403a59cbeae7e88b
CodemakerPHS <pauline.ward5@phs.scot>
2024-09-27 12:20:05 +0100

    functions

--- code/hopr_ch2_functions.R
+++ code/hopr_ch2_functions.R
@@ -5,12 +5,33 @@
 #
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-library(janitor)
+library(janitor) 
+library(openxlsx)
+library(tidyverse)
 
 # 2.2 Objects 
 # Even simple variables that you save are objects. 
 
 a <- 3
-<- <- <- <- <- <- <- 
 
 die <- 1:6
+
+die
+die + 4
+die * 5
+
+die * die
+
+
+# 2.3 Functions 
+
+round_half_up(3.1415)
+
+sample(x = 1:4399, size = 8)
+
+args(round_half_up)
+args(write.xlsx)
+args(ggplot)
+args(str_detect)
+?round_half_up
+?str_detect
