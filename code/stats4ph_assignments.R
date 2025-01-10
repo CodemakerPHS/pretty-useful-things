2affc992482621bb2f1b18bd13d51ef998605b9a
CodemakerPHS <pauline.ward5@phs.scot>
2025-01-10 18:07:56 +0000

    adding stats course script

---
+++ code/stats4ph_assignments.R
@@ -0,0 +1,9 @@
+# My code for doing the work in the assignments in the Coursera specialisation 
+# Stats with R for Public Health 
+
+# Course 1, intro, uses CSV file with six variables, 
+# consumption of fruit and veg
+veg_eg_df <- read_csv("../")
+
+# Course 3, logistic regression, uses CSV file with many variables, 
+# diabetes data
