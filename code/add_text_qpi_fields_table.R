3802e553a75f5b3a50c7655a27df914e48905c05
CodemakerPHS <pauline.ward5@phs.scot>
2026-02-04 12:18:49 +0000

    not working

--- code/add_text_qpi_fields_table.R
+++ code/add_text_qpi_fields_table.R
@@ -2,3 +2,9 @@
 # "QPI Dataset Fields vX.X.xlsx" 
 
 library(openxlsx2)
+
+
+QPI_Dataset_Fields_wb <- openxlsx2::read_xlsx("/PHI_conf/CancerGroup2/Cancer_QPIs/R_individual_folders/pauline_code/input_data/metadata/QPI Dataset Fields v1.0.PW.xlsx")
+
+# not working
+# cancers_sheets <- openxlsx2::wb_get_sheet_names(QPI_Dataset_Fields_wb)
