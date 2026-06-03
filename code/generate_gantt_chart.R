ca3ee544066fca7a7124e2ac04655433bf9e8b7f
CodemakerPHS <pauline.ward5@phs.scot>
2026-06-03 09:41:47 +0000

    outline script to build gantt chart

---
+++ code/generate_gantt_chart.R
@@ -0,0 +1,18 @@
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# 
+# PW planner
+# 
+# 
+#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+library(tidyverse)
+library(openxlsx)
+
+# Prompt the user for the project spec
+# What is the target end / delivery date (ie publication date)? 
+# Do they want to add in all the bog standard QPI dash and publication tasks? 
+# If not, how many generically-named tasks do they want? 
+
+# Include a prominent section for signoff status for each of the networks, 
+# just above the main table, so as to chase up frequently till signed off. 
